fed5825a029738b06dc10219d70fd881d725002d
KimDaJin514 <kimdajin514@gmail.com>
2023-03-27 20:44:40 +0900

    Initial Commit

---
+++ DiaryReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryReview
+//
+//  Created by 김다진 on 2023/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
